44fee217636e7b96ede87cd1c96cec48e429f6e3
youngju <asdadssad@naver.coim>
2018-04-10 15:16:49 +0900

    add Switch

--- DigitalTimer/ViewController.swift
+++ DigitalTimer/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Timerswitch: UISwitch!
     @IBOutlet weak var Timelbl: UILabel!
     var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+//        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-    @objc func updateTime(){
+     func updateTime(){
         let date = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss EE"
@@ -29,7 +31,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
   
-        
+    @IBAction func Switch(_ sender: Any) {
+        if Timerswitch.isOn == true {
+           
+            myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+                self.updateTime()
+            })
+            
+        } else {
+            myTimer.invalidate()
+            
+        }
+    }
+    
       
         
     }
