820557d687ad82a42e14f825e529a8b9cbe22b44
youngju <asdadssad@naver.coim>
2018-04-10 15:00:22 +0900

    Timer

--- DigitalTimer/ViewController.swift
+++ DigitalTimer/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Timelbl: UILabel!
+    var myTimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    @objc func updateTime(){
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EE"
+        Timelbl.text = formatter.string(from: date)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+  
+        
+      
+        
+    }
 
 
-}
 
